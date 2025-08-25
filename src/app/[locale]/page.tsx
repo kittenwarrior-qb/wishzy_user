f85f253ded59fd531df6ad72e2a3c72371efc563
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2025-08-25 22:48:08 +0700

    25/8

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,6 +7,7 @@ import ResultSection from '@/components/pages/home-page/result-section'
 import SearchSection from '@/components/pages/home-page/search-section'
 import TestimonialSection from '@/components/pages/home-page/testimonial-section'
 import TutorialSection from '@/components/pages/home-page/tutorial-section'
+import { Button } from '@/components/ui/button'
 
 export default function HomePage() {
   return (
@@ -18,14 +19,38 @@ export default function HomePage() {
         </div>
       </div>
 
-      <HotListSection/>
-      {/* <CourselistSection /> */}
-      <CategorySection />
-      <AboutSection />
-      <ContactSection />
-      <ResultSection />
-      <TestimonialSection />
-      <TutorialSection />
+      <div className='py-10'>
+        <div className='mx-auto max-w-[1280px] my-5 pb-3'>
+          <p className='font-semibold text-[22px] mb-6'>Khóa Học và Chứng Chỉ</p>
+          <p className='font-semibold text-[30px] mb-3'>Nổi bật trên Wishzy</p>
+          <p className='text-[18px]'>Khám phá các khóa học được nhiều người biết đến, mọi trình độ</p>
+        </div>
+        <HotListSection/>
+
+        <div className='flex gap-3 mx-auto max-w-[1280px] my-5'>
+          <Button variant="default" className='text-[14px]'>
+            Hiển thị thêm
+          </Button>
+          <Button variant="outline" className='text-[14px]'>
+            Xem tất cả
+          </Button>
+        </div>
+      </div>
+
+      <div className='mx-auto max-w-[1280px]'>
+        <CategorySection />
+      </div>
+
+      <div className='mx-auto max-w-[1280px]'>
+        <ResultSection />
+      </div>
+
+      <div className='mx-auto max-w-[1280px] mt-10'>
+        <AboutSection />
+        <ContactSection />
+        <TestimonialSection />
+        <TutorialSection />
+      </div>
     </div>
   )
 }
--- src/components/pages/home-page/result-section.tsx
+++ src/components/pages/home-page/result-section.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ResultSection = () => {
   return (
-    <div>ResultSection</div>
+    <div className='py-20 relative'>
+      <Image src={'https://res.cloudinary.com/djuksxdrw/image/upload/v1754109827/cardpng_nzmt5j.png'} alt='result' width={450} height={450} className='absolute -top-10 -right-10' />
+      <div>
+        hello
+      </div>
+    </div>
   )
 }
 
